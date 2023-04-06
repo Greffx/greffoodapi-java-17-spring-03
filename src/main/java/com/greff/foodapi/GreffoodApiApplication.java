ccdb97d03006f197d6ff92271f65dca774b7d31a
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-06 00:57:17 -0300

    Runner of app

---
+++ src/main/java/com/greff/foodapi/GreffoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.greff.foodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreffoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreffoodApiApplication.class, args);
+	}
+
+}
