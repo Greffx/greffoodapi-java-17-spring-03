92b942a94e8c5f05b2a3b0cc6e3f3bda0043a462
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-06 00:57:38 -0300

    Runner of app test

---
+++ src/test/java/com/greff/foodapi/GreffoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greff.foodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreffoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
