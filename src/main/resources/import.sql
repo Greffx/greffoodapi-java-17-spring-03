96bdcaf9c80fd08ee1ca3426a8771904e3377aa0
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-07 18:13:22 -0300

    Altering to add more attributes at each one of them

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO tb_kitchens (kitchen_name) VALUES ('indian');
-INSERT INTO tb_kitchens (kitchen_name) VALUES ('Japanese');
+INSERT INTO tb_kitchens (id, kitchen_name) VALUES (1, 'indian');
+INSERT INTO tb_kitchens (id, kitchen_name) VALUES (2, 'Japanese');
 
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax) VALUES ('Ganesh_Fast_Food', 3.50);
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax) VALUES ('Haiku_Tales', 5.60);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id) VALUES ('Ganesh_Fast_Food', 3.50, 1);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id) VALUES ('Haiku_Tales', 5.60, 2);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id) VALUES ('SamuraiSushi', 2.50, 2);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id) VALUES ('Spice_Curry_Dish', 4.20, 1);
