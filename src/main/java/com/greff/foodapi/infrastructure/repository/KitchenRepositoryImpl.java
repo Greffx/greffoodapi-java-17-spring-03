49ab934adc204d09ea3d864a2bf7bd23cf79dae1
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-07 15:22:02 -0300

    Class of package infrastructure, like we need to use when there is nothing to do with business rules

---
+++ src/main/java/com/greff/foodapi/infrastructure/repository/KitchenRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.greff.foodapi.infrastructure.repository;
+
+public class KitchenRepositoryImpl {
+    //package Infrastructure, got implementation, like How to access data in db, there's nothing to do with business of app
+    //this class has nothing to do with domain-repository
+    //it's ok to put implementations that got nothing to do with app rules, like business rules
+}
