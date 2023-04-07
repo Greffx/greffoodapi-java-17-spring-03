2c1bc6b1c2ae014e5a614fe4a4aa69636683b57e
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-07 17:47:16 -0300

    more objects to restaurant

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO tb_kitchens (kitchen_name) VALUES ('indian');
 INSERT INTO tb_kitchens (kitchen_name) VALUES ('Japanese');
+
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax) VALUES ('Ganesh_Fast_Food', 3.50);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax) VALUES ('Haiku_Tales', 5.60);
