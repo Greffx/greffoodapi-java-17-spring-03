1dc9ddaf55e3fba15cdcb3bb0be5ff247c100ebf
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-07 17:46:46 -0300

    Adding lombok sintaxe to resume entity

--- src/main/java/com/greff/foodapi/domain/model/Kitchen.java
+++ src/main/java/com/greff/foodapi/domain/model/Kitchen.java
@@ -1,47 +1,20 @@
 package com.greff.foodapi.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.Objects;
-
+@Data//substitute setter of attributes  //substitute getter of attributes
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) //substitute hashcode and equals is to compare objects between them, this means that will choose what will generate hashcode and equals
 @Entity //this entity will turn into a table in DB, that's how we say it
 @Table(name = "tb_kitchens") //that's how to change name of table
 public class Kitchen {
 
+    @EqualsAndHashCode.Include //that is how we choose an attribute to be used in hashcode and equals
     @Id //id of kitchen, say this attribute will represent id of entity
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //to configure how is going to be generatation of id. Receive property 'strategy', will be IDENTITY, given responsibility to SQL to be the provider, it will do id generation for us
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //to configure how is going to be generation of id. Receive property 'strategy', will be IDENTITY, given responsibility to SQL to be the provider, it will do id generation for us
     private Long id;
 
     @Column(name = "kitchen_name") //column map a attribute, can change name
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    //hashcode and equals is to compare objects between them
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Kitchen kitchen = (Kitchen) o;
-        return getId().equals(kitchen.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
--- src/main/java/com/greff/foodapi/domain/model/Restaurant.java
+++ src/main/java/com/greff/foodapi/domain/model/Restaurant.java
@@ -1,14 +1,18 @@
 package com.greff.foodapi.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
 @Entity
 @Table(name = "tb_restaurants")
 public class Restaurant {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,33 +22,4 @@ public class Restaurant {
 
     @Column(name = "delivery_tax")
     private BigDecimal deliveryTax;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurant kitchen = (Restaurant) o;
-        return getId().equals(kitchen.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
