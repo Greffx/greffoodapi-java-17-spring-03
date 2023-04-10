6244ca6d91f7d0f1162e398d93050884d82eaa7b
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-10 14:17:10 -0300

    adding another comment and jsonProperty

--- src/main/java/com/greff/foodapi/domain/model/Kitchen.java
+++ src/main/java/com/greff/foodapi/domain/model/Kitchen.java
@@ -1,5 +1,6 @@
 package com.greff.foodapi.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,9 +13,11 @@ public class Kitchen {
 
     @EqualsAndHashCode.Include //that is how we choose an attribute to be used in hashcode and equals
     @Id //id of kitchen, say this attribute will represent id of entity
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //to configure how is going to be generation of id. Receive property 'STRATEGY', will be IDENTITY, given responsibility to SQL to be the provider, it will do id generation for us
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //to configure how is going to be generation of id. Receive property 'STRATEGY'.
+                                                        //Using IDENTITY, given responsibility to SQL to be the provider, it will do id generation for us
     private Long id;
 
+    @JsonProperty("name") //this not change at domain model, not change the name of attribute. But the representation in JSON will be another, if we change 'name' value
     @Column(name = "kitchen_name", nullable = false) //column map an attribute, can change name, using nullable = false, attribute will not be accepted as null no more
     private String name;
 }
