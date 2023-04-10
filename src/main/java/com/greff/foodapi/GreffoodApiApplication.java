9b9e94f1eb43d538b91a4797441b0bb9b5e1867a
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-10 15:20:17 -0300

    Refactoring

--- src/main/java/com/greff/foodapi/GreffoodApiApplication.java
+++ src/main/java/com/greff/foodapi/GreffoodApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GreffoodApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GreffoodApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GreffoodApiApplication.class, args);
+    }
 }
