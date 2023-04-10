e611004e11b5d9dd55980175547d683ac08e2415
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-10 14:38:01 -0300

    adding comment of how response entity works

--- src/main/java/com/greff/foodapi/api/controller/KitchenController.java
+++ src/main/java/com/greff/foodapi/api/controller/KitchenController.java
@@ -10,29 +10,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController //This annotation is to be recognized as 'controller' and is responsible to receive web request from user, treat them, with services layers and return an answer,
-                //'rest' means that answer should be delivered as a correct answer of http request, without will not work, 'rest' got @ResponseBody annotation inside
-@RequestMapping("/kitchens") //controller needs to be mapped, those requests need to found their right controller, this annotation does it
+@RestController
+//This annotation is to be recognized as 'controller' and is responsible to receive web request from user, treat them, with services layers and return an answer,
+//'rest' means that answer should be delivered as a correct answer of http request, without will not work, 'rest' got @ResponseBody annotation inside
+@RequestMapping("/kitchens")
+//controller needs to be mapped, those requests need to found their right controller, this annotation does it
 public class KitchenController {
 
     private final KitchenService kitchenService; //injection of kitchen service, with the interface type, to be less coupled as possible,
-                                                 //always good to inject interfaces than coupled classes, but if there is no other way, do with the simple type
+
+    //always good to inject interfaces than coupled classes, but if there is no other way, do with the simple type
     public KitchenController(KitchenService kitchenService) {
         this.kitchenService = kitchenService;
     }
 
     @GetMapping()//need to map method, getMapping means that requests with verb http 'GET' will use this method
     public ResponseEntity<List<Kitchen>> getList() { //this is called 'endpoint', with methods like GET, POST, PUT and REMOVE
-                                                     //this can be called as a collection of resources, that means is something that is exposed in web
-                                                     //resource to be reached, need to be id by a URI, we need a URL to request, using http protocol
+        //this can be called as a collection of resources, that means is something that is exposed in web
+        //resource to be reached, need to be id by a URI, we need a URL to request, using http protocol
         return ResponseEntity.ok(kitchenService.findAll());
     }
 
     @GetMapping("/{id}") //This is specification, we need this because can't have 2 endpoints with same path.
-                            //problem with ambiguous endpoints with same URL path "/{can be any other name in here}", that is a placeholder and will bind with our method param 'id'
-                            //with annotation PathVariable, means that will be required to have that id and will do correctly binding part
-                            //this can be called as a singleton resource, that means is something that is exposed in web
-    public ResponseEntity<Kitchen> getKitchen(@PathVariable Long id) {
-        return ResponseEntity.ok(kitchenService.findById(id));
+    //problem with ambiguous endpoints with same URL path "/{can be any other name in here}", that is a placeholder and will bind with our method param 'id'
+    //with annotation PathVariable, means that will be required to have that id and will do correctly binding part
+    //this can be called as a singleton resource, that means is something that is exposed in web
+    public ResponseEntity<Kitchen> getKitchen(@PathVariable Long id) { //ResponseEntity represents HTTP response, which can have an instance of object of some type, ResponseEntity<Type>
+        return ResponseEntity.ok(kitchenService.findById(id)); //it is a builder, has some methods, it means that we can build our response, like ok() return 200 ok status http.
+        //inside (here) is the body response, with that response got payload singleton response or could be a collection resource
     }
 }
