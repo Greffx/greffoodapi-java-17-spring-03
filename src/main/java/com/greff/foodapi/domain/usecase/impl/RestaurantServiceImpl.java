12c3a2449e6a5e5495fc50d17bb88ee42fea60ea
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-11 16:45:59 -0300

    Refactoring findById and getting create method right

--- src/main/java/com/greff/foodapi/domain/usecase/impl/RestaurantServiceImpl.java
+++ src/main/java/com/greff/foodapi/domain/usecase/impl/RestaurantServiceImpl.java
@@ -1,6 +1,8 @@
 package com.greff.foodapi.domain.usecase.impl;
 
+import com.greff.foodapi.domain.model.Kitchen;
 import com.greff.foodapi.domain.model.Restaurant;
+import com.greff.foodapi.domain.repository.KitchenRepository;
 import com.greff.foodapi.domain.repository.RestaurantRepository;
 import com.greff.foodapi.domain.usecase.RestaurantService;
 import com.greff.foodapi.domain.usecase.exception.NotFoundObjectException;
@@ -12,9 +14,11 @@ import java.util.List;
 public class RestaurantServiceImpl implements RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final KitchenRepository kitchenRepository;
 
-    public RestaurantServiceImpl(RestaurantRepository restaurantRepository) {
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, KitchenRepository kitchenRepository) {
         this.restaurantRepository = restaurantRepository;
+        this.kitchenRepository = kitchenRepository;
     }
 
     @Override
@@ -24,11 +28,17 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     public Restaurant findById(Long id) {
-        return restaurantRepository.findById(id).orElseThrow(() -> new NotFoundObjectException(String.format("Restaurant with id %d, not found", id)));
+        return restaurantRepository.findById(id).orElseThrow(() ->
+                new NotFoundObjectException(String.format("Restaurant with id %d, not found", id)));
     }
 
     @Override
     public Restaurant create(Restaurant restaurant) {
-        return null;
+        Long kitchenId = restaurant.getKitchen().getId();
+        Kitchen kitchen = kitchenRepository.findById(kitchenId).orElseThrow(() ->
+                new NotFoundObjectException(String.format("kitchen with id %d, not found", kitchenId)));
+        restaurant.setKitchen(kitchen);
+
+        return restaurantRepository.save(restaurant);
     }
 }
