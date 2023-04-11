254204cb7169472feaa730e5b84674505fc17898
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-11 16:48:42 -0300

    Refactoring

--- src/main/java/com/greff/foodapi/domain/usecase/impl/RestaurantServiceImpl.java
+++ src/main/java/com/greff/foodapi/domain/usecase/impl/RestaurantServiceImpl.java
@@ -1,8 +1,10 @@
 package com.greff.foodapi.domain.usecase.impl;
 
 import com.greff.foodapi.domain.model.Kitchen;
+import com.greff.foodapi.domain.model.PaymentMethod;
 import com.greff.foodapi.domain.model.Restaurant;
 import com.greff.foodapi.domain.repository.KitchenRepository;
+import com.greff.foodapi.domain.repository.PaymentMethodRepository;
 import com.greff.foodapi.domain.repository.RestaurantRepository;
 import com.greff.foodapi.domain.usecase.RestaurantService;
 import com.greff.foodapi.domain.usecase.exception.NotFoundObjectException;
@@ -15,10 +17,12 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
     private final KitchenRepository kitchenRepository;
+    private final PaymentMethodRepository paymentMethodRepository;
 
-    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, KitchenRepository kitchenRepository) {
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, KitchenRepository kitchenRepository, PaymentMethodRepository paymentMethodRepository) {
         this.restaurantRepository = restaurantRepository;
         this.kitchenRepository = kitchenRepository;
+        this.paymentMethodRepository = paymentMethodRepository;
     }
 
     @Override
@@ -35,9 +39,17 @@ public class RestaurantServiceImpl implements RestaurantService {
     @Override
     public Restaurant create(Restaurant restaurant) {
         Long kitchenId = restaurant.getKitchen().getId();
+        Long paymentMethodId = restaurant.getPaymentMethod().getId();
+
         Kitchen kitchen = kitchenRepository.findById(kitchenId).orElseThrow(() ->
                 new NotFoundObjectException(String.format("kitchen with id %d, not found", kitchenId)));
         restaurant.setKitchen(kitchen);
+
+        PaymentMethod paymentMethod = paymentMethodRepository.findById(paymentMethodId).orElseThrow(() ->
+                new NotFoundObjectException(String.format("payment method with id %d, not found", paymentMethodId)));
+
+        restaurant.setKitchen(kitchen);
+        restaurant.setPaymentMethod(paymentMethod);
 
         return restaurantRepository.save(restaurant);
     }
