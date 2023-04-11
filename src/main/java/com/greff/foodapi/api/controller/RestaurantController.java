cdf429200bd32a1fb3a2f51ba88f066f42ebbf4e
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-11 17:07:05 -0300

    Adding update restaurant endpoint in controller

--- src/main/java/com/greff/foodapi/api/controller/RestaurantController.java
+++ src/main/java/com/greff/foodapi/api/controller/RestaurantController.java
@@ -2,6 +2,7 @@ package com.greff.foodapi.api.controller;
 
 import com.greff.foodapi.domain.model.Restaurant;
 import com.greff.foodapi.domain.usecase.RestaurantService;
+ import com.greff.foodapi.domain.usecase.exception.NotFoundObjectException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -29,9 +30,22 @@ public class RestaurantController {
     }
 
     @PostMapping
-    public ResponseEntity<Restaurant> createRestaurant(@RequestBody Restaurant restaurant, UriComponentsBuilder builder) {
-        Restaurant restaurant1 = restaurantService.create(restaurant);
-        return ResponseEntity.created(builder.path("/{id}").buildAndExpand(restaurant1.getId()).toUri()).body(restaurant1);
+    public ResponseEntity<?> createRestaurant(@RequestBody Restaurant restaurant, UriComponentsBuilder builder) { //? is a wildcard, means that can return anything, because of the 38 line, that's a string type, só ? will help with that
+        try {
+            Restaurant restaurant1 = restaurantService.create(restaurant);
+            return ResponseEntity.created(builder.path("/{id}").buildAndExpand(restaurant1.getId()).toUri()).body(restaurant1);
+        } catch (NotFoundObjectException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateRestaurant(@RequestBody Restaurant restaurant, @PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(restaurantService.update(restaurant, id));
+        } catch (NotFoundObjectException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
