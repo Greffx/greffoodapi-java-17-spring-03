54624f079bdb00e5916c6c18c173987b176a7dc5
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-12 16:07:54 -0300

    Adding 2 methods with customized queries by name, prefix flags and such

--- src/main/java/com/greff/foodapi/domain/repository/RestaurantRepository.java
+++ src/main/java/com/greff/foodapi/domain/repository/RestaurantRepository.java
@@ -3,5 +3,15 @@ package com.greff.foodapi.domain.repository;
 import com.greff.foodapi.domain.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    //will query restaurants like DeliveryTax between lowerT >= and biggestT <=
+    //method name is not ok to use it, that expose a lot of attributes and such, is not conventional
+    List<Restaurant> findByDeliveryTaxGreaterThanEqualAndDeliveryTaxLessThanEqual(BigDecimal lowerTax, BigDecimal biggestTax);
+
+    //find by name like 'something' and kitchen id
+    List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchenId);
 }
