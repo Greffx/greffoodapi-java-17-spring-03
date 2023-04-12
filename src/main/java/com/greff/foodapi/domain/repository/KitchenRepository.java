2c33cba313f3c1daadc039e7b04110ddf41f7a32
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-12 15:12:30 -0300

    Adding comment

--- src/main/java/com/greff/foodapi/domain/repository/KitchenRepository.java
+++ src/main/java/com/greff/foodapi/domain/repository/KitchenRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface KitchenRepository extends JpaRepository<Kitchen, Long> { //repository of kitchens, where id entity saying class name and type of id, this is from Spring Data JPA, SDJ
-    //This type of repository of SDJ is to resume boilerplate code, boilerplate - repetitive code
+//Interface extends another interface, where id entity saying class name and type of entity id(like 'long', 'string'), this is from Spring Data JPA, SDJ
+//This type of repository of SDJ is to resume boilerplate code, boilerplate - repetitive code
+public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
     //LIKE means that contains part of a name, '%' joker character, will complete with that :name, ex: if receive letter 'a' will get a list of kitchens that contain letter 'a'
     @Query("SELECT k FROM Kitchen k WHERE lower(k.name) LIKE lower(concat( '%', :name, '%')) ORDER BY k.name DESC")
     //JPQL java Persistence Query Language
