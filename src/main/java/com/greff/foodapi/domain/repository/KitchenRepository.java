4ec2dfc11f6fd4f5103b1faa51491cd6a95f1c7b
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-11 23:10:52 -0300

    Adding JPQL query, customized

--- src/main/java/com/greff/foodapi/domain/repository/KitchenRepository.java
+++ src/main/java/com/greff/foodapi/domain/repository/KitchenRepository.java
@@ -2,8 +2,17 @@ package com.greff.foodapi.domain.repository;
 
 import com.greff.foodapi.domain.model.Kitchen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface KitchenRepository extends JpaRepository<Kitchen, Long> { //repository of kitchens, where id entity saying class name and type of id
+import java.util.List;
+
+public interface KitchenRepository extends JpaRepository<Kitchen, Long> { //repository of kitchens, where id entity saying class name and type of id, this is from Spring Data JPA, SDJ
+
+    //LIKE means that contains part of a name, '%' joker character, will complete with that :name, ex: if receive letter 'a' will get a list of kitchens that contain letter 'a'
+    @Query("SELECT k FROM Kitchen k WHERE lower(k.name) LIKE lower(concat( '%', :name, '%')) ORDER BY k.name DESC") //JPQL java Persistence Query Language
+    List<Kitchen> findByName(@Param("name") String name); //@Param makes binding of name with :name up there
+
 }
 
 //repository is to be in domain level
