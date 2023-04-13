bca2b4895761fe0bc0e3672c3e36b5026924c503
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-13 18:11:36 -0300

    Adding comments

--- src/main/java/com/greff/foodapi/domain/model/Restaurant.java
+++ src/main/java/com/greff/foodapi/domain/model/Restaurant.java
@@ -34,6 +34,7 @@ public class Restaurant {
     //many restaurants owns many payment methods
     @ManyToMany
     //@JoinTable(name = "nameOfIntermediateClass") altering name of class that will be needed it in @ManyToMany association
+    //when we do this, intermediate table will be created in db
     //joinColumns defines columns of foreign key in intermediate class that defines restaurant.
     //like if intermediateClass has attribute like 'restaurantId', we need to define this in joinColumns
     //it needs to know which is the name column of foreign key in intermediateClass that represents restaurant
