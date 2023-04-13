f6ac937113c02c42cde0007557ccd74920c01175
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-13 14:19:26 -0300

    Adding comments and commeting a query to use in factory specifications

--- src/main/java/com/greff/foodapi/domain/repository/RestaurantRepository.java
+++ src/main/java/com/greff/foodapi/domain/repository/RestaurantRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,12 +19,13 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, R
     //method name is not ok to use it, that expose a lot of attributes and entities, is not conventional.
     // ex: queryByDeliveryTaxGreaterThanEqualAndDeliveryTaxLessThanEqual
     //others prefixes can be used, as 'READ', 'GET', 'QUERY' or 'STREAM', they all do the same thing as find
-    @Query("FROM Restaurant WHERE deliveryTax >= :lowerTax AND  deliveryTax <= :higherTax")
-    List<Restaurant> searchTaxByLowerTaxAndHigherTax(BigDecimal lowerTax, BigDecimal higherTax);
+    //@Query("FROM Restaurant WHERE deliveryTax >= :lowerTax AND  deliveryTax <= :higherTax")
+    //List<Restaurant> searchTaxByLowerTaxAndHigherTax(BigDecimal lowerTax, BigDecimal higherTax)
 
     //in this annotation can pass a JPQL query
-    @Query("FROM Restaurant WHERE name LIKE %:name% AND kitchen.id = :id") //it means 'from tb restaurant where name is contains 'varName' and kitchen id equals to 'paramId'
-        // find by name like 'something' and kitchen id, Param can make bind with another name to use in annotation
+    @Query("FROM Restaurant WHERE name LIKE %:name% AND kitchen.id = :id")
+    //it means 'from tb restaurant where name is contains 'varName' and kitchen id equals to 'paramId'
+    // find by name like 'something' and kitchen id, Param can make bind with another name to use in annotation
     List<Restaurant> searchByNameKitchenId(String name, @Param("id") Long kitchenId);
 
     //will get the first restaurant that get name like string 'name', making 'LIKE' and limiting at same time
