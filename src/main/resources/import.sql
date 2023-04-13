63545a5b757d4e1372175c28bbd8cc40c96b4a5b
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-13 18:10:34 -0300

    Adding inserts into intermediate class of payments and restaurants

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,12 @@ INSERT INTO tb_kitchens (id, kitchen_name) VALUES (1, 'indian');
 INSERT INTO tb_kitchens (id, kitchen_name) VALUES (2, 'Japanese');
 INSERT INTO tb_kitchens (id, kitchen_name) VALUES (3, 'Brazilian');
 
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code) VALUES ('Ganesh_Fast_Food', 3.50, 1);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code) VALUES ('Haiku_Tales', 5.60, 2);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code) VALUES ('SamuraiSushi', 2.50, 2);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code) VALUES ('Indian_Hero', 3.20, 1);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code) VALUES ('Speed Galeto BR', 0, 3);
+INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code) VALUES ('Marmita Do Zé', 0, 3);
 
 INSERT INTO tb_states (id, state_name) VALUES (1, 'RS');
 INSERT INTO tb_states (id, state_name) VALUES (2, 'PR');
@@ -19,10 +25,5 @@ INSERT INTO tb_permissions (id, name, description) VALUES (1, 'consult of produc
 INSERT INTO tb_permissions (id, name, description) VALUES (2, 'consult of states', 'this user got permission to see states');
 INSERT INTO tb_permissions (id, name, description) VALUES (3, 'consult of restaurants', 'this user got permission to see restaurants');
 
+INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (1,1), (1,2), (1,3), (2,1), (2,2), (3,2), (3,3);
 
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code, payment_method_id) VALUES ('Ganesh_Fast_Food', 3.50, 1, 1);
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code, payment_method_id) VALUES ('Haiku_Tales', 5.60, 2, 1);
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code, payment_method_id) VALUES ('SamuraiSushi', 2.50, 2, 2);
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code, payment_method_id) VALUES ('Indian_Hero', 3.20, 1, 3);
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code, payment_method_id) VALUES ('Speed Galeto BR', 0, 3, 3);
-INSERT INTO tb_restaurants (restaurant_name, delivery_tax, kitchen_id_code, payment_method_id) VALUES ('Marmita Do Zé', 0, 3, 3);
