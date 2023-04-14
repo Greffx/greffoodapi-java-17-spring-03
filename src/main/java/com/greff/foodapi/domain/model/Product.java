442e1c8be3807a5895041acd4dc67733eb775bb3
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-14 19:59:58 -0300

    Entity product with relationship many to one with restaurant

---
+++ src/main/java/com/greff/foodapi/domain/model/Product.java
@@ -0,0 +1,29 @@
+package com.greff.foodapi.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+}
