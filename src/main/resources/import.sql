06b7a9997b1ed0215bb72e0b4bda9b72a8e7ca7d
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-14 21:02:41 -0300

    Adding insertions of products, users, groups and their many to many relactions tables too

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -29,24 +29,45 @@ INSERT INTO tb_payment_methods (id, description) VALUES (1, 'cash');
 INSERT INTO tb_payment_methods (id, description) VALUES (2, 'credit card');
 INSERT INTO tb_payment_methods (id, description) VALUES (3, 'debit card');
 
+#Relation many to many with restaurants and payments
+#that's why we got a table here with that information
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (1,1);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (1,2);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (1,3);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (2,1);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (2,2);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (3,2);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (3,3);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (4,1);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (5,2);
+INSERT INTO tb_restaurants__payment_methods (restaurant_id, payment_method_id) VALUES (6,1);
+
+#Relation many products to one restaurant
+INSERT INTO tb_products (active, description, name, price, restaurant_id) VALUES (true, 'goes with a delicious cream', 'Salad', 15, 1);
+INSERT INTO tb_products (active, description, name, price, restaurant_id) VALUES (true, 'goes with a soda and fries', 'Vegan Burger', 45.20, 3);
+INSERT INTO tb_products (active, description, name, price, restaurant_id) VALUES (true, 'Most fast and worm food piece', 'Pizza slice', 7.45, 2);
+
 INSERT INTO tb_permissions (id, name, description) VALUES (1, 'consult of products', 'this user got permission to see products');
 INSERT INTO tb_permissions (id, name, description) VALUES (2, 'consult of states', 'this user got permission to see states');
 INSERT INTO tb_permissions (id, name, description) VALUES (3, 'consult of restaurants', 'this user got permission to see restaurants');
 
-#Relation many to many with restaurants and payments
-#that's why we got a table here with that information
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (1,1);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (1,2);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (1,3);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (2,1);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (2,2);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (3,2);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (3,3);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (4,1);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (5,2);
-INSERT INTO tb_restaurant__payment_method (restaurant_id, payment_method_id) VALUES (6,1);
+INSERT INTO tb_users(email, name, password, register_date) VALUES ('bob23@gmail.com', 'bob', '1234', utc_timestamp);
+INSERT INTO tb_users(email, name, password, register_date) VALUES ('alex24@gmail.com', 'alex', '1232', utc_timestamp);
+INSERT INTO tb_users(email, name, password, register_date) VALUES ('john25@gmail.com', 'john', '1235', utc_timestamp);
+INSERT INTO tb_users(email, name, password, register_date) VALUES ('hawk26@gmail.com', 'hawk', '1231', utc_timestamp);
 
-#Relation many products to one restaurant
-INSERT INTO tb_product (active, description, name, price, restaurant_id) VALUES (true, 'goes with a delicious cream', 'Salad', 15, 1);
-INSERT INTO tb_product (active, description, name, price, restaurant_id) VALUES (true, 'goes with a soda and fries', 'Vegan Burger', 45.20, 3);
-INSERT INTO tb_product (active, description, name, price, restaurant_id) VALUES (true, 'Most fast and worm food piece', 'Pizza slice', 7.45, 2);
+INSERT INTO tb_groups(id, description) VALUES (1, 'adm');
+INSERT INTO tb_groups(id, description) VALUES (2, 'user');
+
+#Relation many to many with groups and permissions
+INSERT INTO tb_groups_permissions(group_id, permission_id) VALUES (1, 1);
+INSERT INTO tb_groups_permissions(group_id, permission_id) VALUES (1, 2);
+INSERT INTO tb_groups_permissions(group_id, permission_id) VALUES (1, 3);
+INSERT INTO tb_groups_permissions(group_id, permission_id) VALUES (2, 1);
+INSERT INTO tb_groups_permissions(group_id, permission_id) VALUES (2, 3);
+
+#Relation many to many with groups and users
+INSERT INTO tb_users_groups(user_id, group_id) VALUES (1, 1);
+INSERT INTO tb_users_groups(user_id, group_id) VALUES (2, 1);
+INSERT INTO tb_users_groups(user_id, group_id) VALUES (3, 2);
+INSERT INTO tb_users_groups(user_id, group_id) VALUES (4, 2);
