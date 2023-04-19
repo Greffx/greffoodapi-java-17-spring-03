d277eebf8615ffa6c3750ed4aee9867cb26b1b88
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-19 19:10:20 -0300

    migrations to order table and orderItem table

---
+++ src/main/resources/db/migration/V011__create_tb_orders.sql
@@ -0,0 +1,39 @@
+CREATE TABLE tb_orders
+(
+    id                  BIGINT AUTO_INCREMENT NOT NULL,
+    subtotal            DECIMAL(9, 2)         NOT NULL,
+    delivery_tax        DECIMAL(9, 2)         NOT NULL,
+    total               DECIMAL(9, 2)         NOT NULL,
+    creation_date       DATETIME              NOT NULL,
+    confirmed_date      DATETIME,
+    canceled_date       DATETIME,
+    delivered_date      DATETIME,
+    restaurant_id       BIGINT                NOT NULL,
+    payment_method_id   BIGINT                NOT NULL,
+    user_id             BIGINT                NOT NULL,
+    city_address_id     BIGINT                NOT NULL,
+    cep_address         VARCHAR(8),
+    complement_address  VARCHAR(255),
+    number_address      VARCHAR(20),
+    public_area_address VARCHAR(255),
+    street_address      VARCHAR(80),
+
+    CONSTRAINT pk_tb_orders PRIMARY KEY (id)
+);
+ALTER TABLE tb_orders
+    ADD CONSTRAINT fk_tb_orders_restaurants FOREIGN KEY (restaurant_id) REFERENCES tb_restaurants (id);
+
+ALTER TABLE tb_orders
+    ADD CONSTRAINT fk_tb_orders_payment_methods FOREIGN KEY (payment_method_id) REFERENCES tb_payment_methods (id);
+
+ALTER TABLE tb_orders
+    ADD CONSTRAINT fk_tb_orders_users FOREIGN KEY (user_id) REFERENCES tb_users (id);
+
+ALTER TABLE tb_orders
+    ADD CONSTRAINT fk_tb_orders_cities_address FOREIGN KEY (city_address_id) REFERENCES tb_cities (id);
+
+#ADD CONSTRAINT is like adding a nickname, FOREIGN KEY is referencing property name in <tableName> to be a foreign key and making reference
+# to another table id column
+
+#CONSTRAINT in primary ket is the same thing, just a nike name, a index name
+#when we want to create a FK(foreign key) need to create property at table, with bigint to be a legit idz
---
+++ src/main/resources/db/migration/V012__create_tb_order_items.sql
@@ -0,0 +1,21 @@
+CREATE TABLE tb_order_items
+(
+    id          BIGINT AUTO_INCREMENT NOT NULL,
+    quantity    INT                   NOT NULL,
+    unity_price DECIMAL(9, 2)         NOT NULL,
+    total       DECIMAL(9, 2)         NOT NULL,
+    observation VARCHAR(255),
+    order_id    BIGINT                NOT NULL,
+    product_id  BIGINT                NOT NULL,
+
+    CONSTRAINT pk_tb_orders_items PRIMARY KEY (id)
+);
+
+ALTER TABLE tb_order_items
+    ADD CONSTRAINT fk_tb_orders_items_products FOREIGN KEY (product_id) REFERENCES tb_products (id);
+
+#ADD CONSTRAINT is like adding a nickname, FOREIGN KEY is referencing property name in <tableName> to be a foreign key and making reference
+# to another table id column
+
+#CONSTRAINT in primary ket is the same thing, just a nike name, a index name
+#when we want to create a FK(foreign key) need to create property at table, with bigint to be a legit idz
