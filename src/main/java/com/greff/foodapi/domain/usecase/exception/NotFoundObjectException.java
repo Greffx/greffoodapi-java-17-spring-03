ef64b681e8b18e42512c9f64d959d98d334fb7f1
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-20 11:40:26 -0300

    Refactoring herit and adding another constructor

--- src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
@@ -1,7 +1,19 @@
 package com.greff.foodapi.domain.usecase.exception;
 
-public class NotFoundObjectException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
+
+//ResponseStatusException this exception is from springframework, it's a runTimeException too
+//in hierarchy classes, ResponseStatusException got subclasses that we can use for which response that we need
+public class NotFoundObjectException extends ResponseStatusException {
+
+    public NotFoundObjectException(HttpStatusCode status, String reason) { //created a constructor that receives 2 params
+        super(status, reason); //and repass to super constructor, which is from parent class
+    }
+
     public NotFoundObjectException(String message) {
-        super(message);
+        this(HttpStatus.NOT_FOUND, message); //this will call line 12
+        //if this class got instanced and get this constructor right here the status code will be not found by default but can inform another too
     }
 }
