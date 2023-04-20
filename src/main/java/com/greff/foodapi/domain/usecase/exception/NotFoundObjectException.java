a6b5d43911fb8527d45e5baebe63c69f838032d4
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-20 16:35:37 -0300

    Refactoring just with annotation of responseStatus to it

--- src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
@@ -1,5 +1,9 @@
 package com.greff.foodapi.domain.usecase.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundObjectException extends RuntimeException {
 
     public NotFoundObjectException(String message) {
