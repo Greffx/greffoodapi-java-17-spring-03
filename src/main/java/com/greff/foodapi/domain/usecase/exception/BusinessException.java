aae28bc4e96f9fb26d6053dda4f9f9d5e87044c4
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-21 22:32:01 -0300

    adding another constructor to throw cause too

--- src/main/java/com/greff/foodapi/domain/usecase/exception/BusinessException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/BusinessException.java
@@ -10,4 +10,8 @@ public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
     }
+
+    public BusinessException(String message, Throwable cause) { //Throwable first of all exceptions, big parent one here
+        super(message, cause);
+    }
 }
