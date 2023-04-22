71f46b0e45b13280cd715f646308608b885ed694
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-21 23:16:05 -0300

    adding comments

--- src/main/java/com/greff/foodapi/domain/usecase/exception/StateNotFoundException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/StateNotFoundException.java
@@ -3,6 +3,7 @@ package com.greff.foodapi.domain.usecase.exception;
 //inheriting NotFoundObjectException, starting to create a hierarchy of exceptions
 //it's cool because state not found it's an object not found
 //since this heritage already got annotation not found, we don't need to put, but if you want to be more specific should use it
+//that's what they call 'granularidade fina', that means more detailed type, rich in explanation
 public class StateNotFoundException extends NotFoundObjectException {
 
     public StateNotFoundException(String message) { //receive msg into constructor type and pass to super constructor, parent that got extended
