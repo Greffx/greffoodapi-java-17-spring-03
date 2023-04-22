62fa9c100dec96b3f635609f225e9b1a8f058ae7
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-21 23:20:31 -0300

    adding this constructor to alter msg

--- src/main/java/com/greff/foodapi/domain/usecase/exception/EntityInUseException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/EntityInUseException.java
@@ -9,4 +9,8 @@ public class EntityInUseException extends BusinessException {
     public EntityInUseException(String message) {
         super(message);
     }
+
+    public EntityInUseException(String entityName, Long id) {
+        this(String.format("%s with id %d can't be removed because still been used", entityName, id));
+    }
 }
