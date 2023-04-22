72631e917337489c02dcf865d04b4695e50693db
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-22 16:14:22 -0300

    refactoring by removing annotation with response status, changing by handler classes to do that

--- src/main/java/com/greff/foodapi/domain/usecase/exception/BusinessException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/BusinessException.java
@@ -1,10 +1,6 @@
 package com.greff.foodapi.domain.usecase.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 //general error for business, can be used in anytype of error of business
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BusinessException extends RuntimeException {
 
     public BusinessException(String message) {
--- src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
@@ -1,9 +1,5 @@
 package com.greff.foodapi.domain.usecase.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public abstract class NotFoundObjectException extends BusinessException {
     //we can not instance more this type of exception, serves to help others
     //but can be used in catch, so if needed to catch something more general, can use this
