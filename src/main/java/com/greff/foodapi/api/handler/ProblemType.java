84dc62378e0f0b61b6906d1d2d0b573fdbaf3cdc
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-24 17:03:36 -0300

    adding to HttpMessageNotReadable to global handler and enum type

--- src/main/java/com/greff/foodapi/api/handler/ProblemType.java
+++ src/main/java/com/greff/foodapi/api/handler/ProblemType.java
@@ -8,7 +8,8 @@ public enum ProblemType {
     ENTITY_NOT_FOUND("/entity-not-found", "Entity not found"), //constant to be used in problemDetails
     //that way we don't need to do it in customHandler, if there's more than 100 methods, we would need to write each one, that way, only use constant enum
     ENTITY_IN_USE("/entity-in-use", "Entity is been used"),
-    INVALID_DATA("/invalid-data", "Invalid data");
+    INVALID_DATA("/invalid-data", "Invalid data"),
+    INVALID_MESSAGE("/invalid-message", "Invalid message");
 
     private final String title; //type of problem title
     private final String uri; //type of problem uri
