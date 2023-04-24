6df0b060c4ce7cb1cc6d35759418c909145eda8c
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-24 16:04:33 -0300

    ENUM of problems to use constant in global handlers

---
+++ src/main/java/com/greff/foodapi/api/handler/ProblemType.java
@@ -0,0 +1,18 @@
+package com.greff.foodapi.api.handler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+
+    ENTITY_NOT_FOUND ("/entity-not-found", "Entity not found"); //constant to be used in problemDetails
+    //that way we don't need to do it in customHandler, if there's more than 100 methods, we would need to write each one, that way, only use constant enum
+
+    private final String title; //type of problem title
+    private final String uri; //type of problem uri
+
+    ProblemType(String path, String title) {
+        this.uri = "https://greffood.com.br" + path;
+        this.title = title;
+    }
+}
