65eb753bbf6904ff056f9f1b201a4e7540c87e5e
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-26 16:08:20 -0300

    refactoring to another name

--- src/main/java/com/greff/foodapi/api/handler/ProblemType.java
+++ src/main/java/com/greff/foodapi/api/handler/ProblemType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-    ENTITY_NOT_FOUND("/entity-not-found", "Entity not found"), //constant to be used in problemDetails
+    RESOURCE_NOT_FOUND("/resource-not-found", "Resource not found"), //constant to be used in problemDetails
     //that way we don't need to do it in customHandler, if there's more than 100 methods, we would need to write each one, that way, only use constant enum
     ENTITY_IN_USE("/entity-in-use", "Entity is been used"),
     INVALID_DATA("/invalid-data", "Invalid data"),
