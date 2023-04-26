5db4ecbf02d58f2f1c8aeed5f7427b7e638afcd3
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-26 17:20:20 -0300

    adding implementation to internal error, uncaught error, with excpetion

--- src/main/java/com/greff/foodapi/api/handler/ApiExceptionHandler.java
+++ src/main/java/com/greff/foodapi/api/handler/ApiExceptionHandler.java
@@ -72,6 +72,23 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, problemDetails, new HttpHeaders(), status, request);
     }
 
+    //method to treat every exception that got no treat in another method
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUncaughtException(Exception ex, WebRequest request) {
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ProblemType problemType = ProblemType.INTERNAL_ERROR;
+        String detail = "Internal error unexpected, try again and if happens the same problem, contact system administrator.";
+
+        ProblemDetails problemDetails = createProblemDetailsBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problemDetails, new HttpHeaders(), status, request);
+
+    }
+
+
+        //type of error when JSON got something wrong, like a 'localhost/restaurant/1s' using an id long as a string
+    //but this is a general method, if is an instance of a child class, will throw others classes
     @Override
     protected ResponseEntity<Object> handleTypeMismatch(
             TypeMismatchException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
@@ -87,11 +104,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, problemDetails, headers, status, request);
     }
 
+    //exception thrown when a parameter of URL of body is wrong type
     private ResponseEntity<Object> handleMethodArgumentTypeMismatchException(
             MethodArgumentTypeMismatchException subEx, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
         ProblemType problemType = ProblemType.INVALID_PARAMETER;
-            String detail = String.format("URL parameter '%s', with value '%s' is a invalid type. Try again using %s type", subEx.getParameter().getParameterName(),
+            String detail = String.format("Parameter '%s', with value '%s' is a invalid type. Try again using %s type", subEx.getParameter().getParameterName(),
                     subEx.getValue(), Objects.requireNonNull(subEx.getRequiredType()).getSimpleName());
 
         ProblemDetails problemDetails = createProblemDetailsBuilder(HttpStatus.valueOf(status.value()), problemType, detail).build();
@@ -100,7 +118,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     }
 
-    @Override //type of error when JSON got something wrong, like a ',() {}' in wrong place or something like that or string instead of integer when needed an integer
+    //type of error when JSON got something wrong, like a ',() {}' in wrong place or something like that or string instead of integer when needed an integer
+    //but this is a general method, if is an instance of a child class, will throw others classes
+    @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(
             HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         Throwable rootCause = ExceptionUtils.getRootCause(ex); //goes through entire exception stack and get root exception, root cause
@@ -117,6 +137,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, problemDetails, headers, status, request);
     }
 
+    //exception is when using in request body property that's unknown or ignored
     private ResponseEntity<Object> handlePropertyBindingException(
             PropertyBindingException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
@@ -130,8 +151,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, problemDetails, headers, status, request);
     }
-    //type of error when JSON got something wrong, like string type instead of integer type, string id instead of long id
 
+    //type of error when JSON got something wrong, like string type instead of integer type, string id instead of long id
     private ResponseEntity<Object> handleInvalidFormatException(
             InvalidFormatException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
@@ -147,7 +168,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, problemDetails, headers, status, request);
     }
 
-    @Override
+    @Override //exception when there's no exception to handle
     protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
         ProblemType problemType = ProblemType.RESOURCE_NOT_FOUND;
@@ -178,6 +199,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 
+    //method to create a builder of problemDetails, instance ONLY A BUILDER, it isn't built yet, will come as a builder, not an instance of problemDetails
     private ProblemDetails.ProblemDetailsBuilder createProblemDetailsBuilder(
             HttpStatus status, ProblemType problemType, String detail) { //method to instance a builder
         //will not instance a build(); only builder of problemDetails, build must be done in method, case that need something else, can add in there
