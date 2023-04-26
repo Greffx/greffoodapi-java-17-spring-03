fcc22bcb5c03a766c635e355a04d9f9bf1bbf8bb
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-26 15:28:59 -0300

    Adding another type

--- src/main/java/com/greff/foodapi/api/handler/ProblemType.java
+++ src/main/java/com/greff/foodapi/api/handler/ProblemType.java
@@ -9,7 +9,8 @@ public enum ProblemType {
     //that way we don't need to do it in customHandler, if there's more than 100 methods, we would need to write each one, that way, only use constant enum
     ENTITY_IN_USE("/entity-in-use", "Entity is been used"),
     INVALID_DATA("/invalid-data", "Invalid data"),
-    INVALID_MESSAGE("/invalid-message", "Invalid message");
+    INVALID_MESSAGE("/invalid-message", "Invalid message"),
+    INVALID_PARAMETER("/invalid-parameter", "Invalid parameter");
 
     private final String title; //type of problem title
     private final String uri; //type of problem uri
