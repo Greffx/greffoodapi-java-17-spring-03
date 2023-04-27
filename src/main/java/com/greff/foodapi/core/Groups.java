025ab45a5d01541e671d0006e9075b3a45a42904
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-27 19:59:10 -0300

    refactoring group name

--- src/main/java/com/greff/foodapi/core/Groups.java
+++ src/main/java/com/greff/foodapi/core/Groups.java
@@ -3,5 +3,5 @@ package com.greff.foodapi.core;
 public interface Groups { //validation group, receives an array os types, like class, interface.
     //could be a class, just need to be a type
 
-    interface RestaurantRegister {} //marking type
+    interface KitchenId {} //marking type
 }
