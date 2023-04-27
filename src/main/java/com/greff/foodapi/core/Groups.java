c1f500fed738d9d8ecdcdaddf3e8b94305624fe8
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-27 19:34:46 -0300

    Creatiang groups to validation annotations marking at entities

---
+++ src/main/java/com/greff/foodapi/core/Groups.java
@@ -0,0 +1,7 @@
+package com.greff.foodapi.core;
+
+public interface Groups { //validation group, receives an array os types, like class, interface.
+    //could be a class, just need to be a type
+
+    interface RestaurantRegister {} //marking type
+}
