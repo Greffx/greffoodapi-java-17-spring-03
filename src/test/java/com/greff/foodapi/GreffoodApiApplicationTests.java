944feb69a1ea77d7bf6041de76d3449e575e48df
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-28 17:50:15 -0300

    Adding comments about tests

--- src/test/java/com/greff/foodapi/GreffoodApiApplicationTests.java
+++ src/test/java/com/greff/foodapi/GreffoodApiApplicationTests.java
@@ -1,12 +1,14 @@
 package com.greff.foodapi;
 
+//Junit is a simple library to execute test code, can write API and integration tests too
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //supply with spring boot functions for tests
 class GreffoodApiApplicationTests {
 
-	@Test
+	@Test //every test method has this annotation, that's how is recognized as a test method
 	void contextLoads() {
 	}
 
