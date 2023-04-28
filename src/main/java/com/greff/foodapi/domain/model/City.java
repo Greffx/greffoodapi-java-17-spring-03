88ec2342ab988a2b305129dc2932b541b21de341
Eduardo Greff <eduardogreff0@gmail.com>
2023-04-28 13:58:48 -0300

    adding validation annotations and groups to properties

--- src/main/java/com/greff/foodapi/domain/model/City.java
+++ src/main/java/com/greff/foodapi/domain/model/City.java
@@ -1,6 +1,11 @@
 package com.greff.foodapi.domain.model;
 
+import com.greff.foodapi.core.Groups;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.ConvertGroup;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -15,9 +20,13 @@ public class City {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String name;
 
+    @NotNull
+    @Valid
+    @ConvertGroup(to = Groups.StateId.class)
     @ManyToOne
     @JoinColumn(name = "state_id", nullable = false)
     private State state;
--- src/main/java/com/greff/foodapi/domain/model/State.java
+++ src/main/java/com/greff/foodapi/domain/model/State.java
@@ -1,6 +1,9 @@
 package com.greff.foodapi.domain.model;
 
+import com.greff.foodapi.core.Groups;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,11 +13,13 @@ import lombok.EqualsAndHashCode;
 @Table(name = "tb_states")
 public class State {
 
+    @NotNull(groups = Groups.StateId.class)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String name;
 }
