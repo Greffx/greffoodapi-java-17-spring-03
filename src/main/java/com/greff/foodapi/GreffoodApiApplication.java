f3e1064b897902675d25aed202474222f718d1af
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-04 18:04:39 -0300

    changing timezone of application to UTC type

--- src/main/java/com/greff/foodapi/GreffoodApiApplication.java
+++ src/main/java/com/greff/foodapi/GreffoodApiApplication.java
@@ -3,10 +3,13 @@ package com.greff.foodapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GreffoodApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC")); //changing timezone of application to UTC type
         SpringApplication.run(GreffoodApiApplication.class, args);
     }
 }
