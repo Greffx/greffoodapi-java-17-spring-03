a6a1412089bf69230f9ac150e65f155daf2d60ec
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-05 19:42:55 -0300

    Not using anymore, but good to know, more knowledge

--- src/main/java/com/greff/foodapi/core/validation/Groups.java
+++ src/main/java/com/greff/foodapi/core/validation/Groups.java
@@ -1,9 +1,9 @@
-package com.greff.foodapi.core.validation; //core is a package that got nothing to do with domain, infra, api, so everything that got nothing, going in here
-
-public interface Groups { //validation group, receives an array os types, like class, interface.
-    //could be a class, just need to be a type
-
-    interface KitchenId {} //marking type
-
-    interface StateId {}
-}
+//package com.greff.foodapi.core.validation; //core is a package that got nothing to do with domain, infra, api, so everything that got nothing, going in here
+//
+//public interface Groups { //validation group, receives an array os types, like class, interface.
+//    //could be a class, just need to be a type
+//
+//    interface KitchenId {} //marking type
+//
+//    interface StateId {}
+//}
