e27615f99402e142fed6ed920a165d454215ce97
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-05 19:13:24 -0300

    JSON ignores to mix-in class of kitchen

---
+++ src/main/java/com/greff/foodapi/api/model/mixin/KitchenMixin.java
@@ -0,0 +1,16 @@
+package com.greff.foodapi.api.model.mixin;
+//DOMAIN LAYER OF DDD pattern - domain model: got models/entities, - got repositories interfaces, and - got services with business rules implementations
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.greff.foodapi.domain.model.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KitchenMixin {
+
+
+    @JsonIgnore //means to ignore this property when serializing kitchen in responses
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+}
