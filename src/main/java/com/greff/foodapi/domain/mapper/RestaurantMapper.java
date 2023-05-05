830542d6480ccb74986e8e11fec2bb3492270efc
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-05 15:52:33 -0300

    Mapstruct interface to map entity and DTOs

---
+++ src/main/java/com/greff/foodapi/domain/mapper/RestaurantMapper.java
@@ -0,0 +1,16 @@
+package com.greff.foodapi.domain.mapper;
+
+import com.greff.foodapi.api.model.response.RestaurantResponse;
+import com.greff.foodapi.domain.model.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Service;
+
+@Mapper(componentModel = "spring") //@Mapper - annotation tha marks interface as mapping interface to spring
+@Service
+public interface RestaurantMapper {
+
+     //since those objects got different names, need to map target and source to change values
+     @Mapping(target = "kitchenResponse", source = "kitchen")
+     RestaurantResponse fromRestaurantToRestaurantResponse(Restaurant restaurant); //method to transform entity to DTO representation
+}
