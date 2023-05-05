07442d5add8e4c6cc161c1deb6bf48f3e578d580
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-05 15:52:50 -0300

    Creating Kitchen response DTO

---
+++ src/main/java/com/greff/foodapi/api/model/response/KitchenResponse.java
@@ -0,0 +1,12 @@
+package com.greff.foodapi.api.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class KitchenResponse {
+
+    private Long id;
+    private String name;
+}
