6a12b2bcf0b036e2bcac04697af341c968a73fd4
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-08 14:36:04 -0300

    Altering representation to use DTOs instead of original entity

--- src/main/java/com/greff/foodapi/api/controller/CityController.java
+++ src/main/java/com/greff/foodapi/api/controller/CityController.java
@@ -1,22 +1,24 @@
 package com.greff.foodapi.api.controller;
 
+import com.greff.foodapi.api.model.request.CityRequest;
+import com.greff.foodapi.api.model.response.CityResponse;
+import com.greff.foodapi.domain.mapper.CityMapper;
 import com.greff.foodapi.domain.model.City;
 import com.greff.foodapi.domain.usecase.CityService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/cities")
 public class CityController {
 
     private final CityService cityService;
-
-    public CityController(CityService cityService) {
-        this.cityService = cityService;
-    }
+    private final CityMapper cityMapper;
 
     @GetMapping
     public List<City> findAll() {
@@ -30,13 +32,19 @@ public class CityController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public City createCity(@RequestBody @Valid City city) {
-        return cityService.create(city);
+    public CityResponse createCity(@RequestBody @Valid CityRequest cityRequest) {
+        var city = cityMapper.fromCityRequestToCity(cityRequest);
+        var cityResponse = cityService.create(city);
+
+        return cityMapper.fromCityToCityResponse(cityResponse);
     }
 
     @PutMapping("/{id}")
-    public City updateCity(@RequestBody @Valid City city, @PathVariable Long id) {
-        return cityService.update(city, id);
+    public CityResponse updateCity(@RequestBody @Valid CityRequest cityRequest, @PathVariable Long id) {
+        var city = cityMapper.fromCityRequestToCity(cityRequest);
+        var cityResponse =  cityService.update(city, id);
+
+        return cityMapper.fromCityToCityResponse(cityResponse);
     }
 
     @DeleteMapping("/{id}")
