46a45016d94321d25469c8c71915aa881388e22e
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-08 15:13:54 -0300

    Assembler to model and disassembler to domain object

---
+++ src/main/java/com/greff/foodapi/api/assembler/RestaurantAssembler.java
@@ -0,0 +1,19 @@
+package com.greff.foodapi.api.assembler; //assembler is like a builder, converter, transformer
+//package used to transform entity to model representation
+
+import com.greff.foodapi.api.model.response.RestaurantResponse;
+import com.greff.foodapi.domain.mapper.RestaurantMapper;
+import com.greff.foodapi.domain.model.Restaurant;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component //to work, needs to be a spring component
+public class RestaurantAssembler {
+
+    private final RestaurantMapper restaurantMapper;
+
+    public RestaurantResponse toModel(Restaurant restaurant) {
+        return restaurantMapper.fromRestaurantToRestaurantResponse(restaurant);
+    }
+}
---
+++ src/main/java/com/greff/foodapi/api/assembler/RestaurantRequestDisassembler.java
@@ -0,0 +1,19 @@
+package com.greff.foodapi.api.assembler;
+
+import com.greff.foodapi.api.model.request.RestaurantRequest;
+import com.greff.foodapi.domain.mapper.RestaurantMapper;
+import com.greff.foodapi.domain.model.Restaurant;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+public class RestaurantRequestDisassembler {
+    //class used to transform model representation to domain object
+
+    private final RestaurantMapper restaurantMapper;
+
+    public Restaurant toDomainObject(RestaurantRequest restaurantRequest) {
+        return restaurantMapper.fromRestaurantRequestToRestaurant(restaurantRequest);
+    }
+}
