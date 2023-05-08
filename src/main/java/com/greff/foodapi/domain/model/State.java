9312e849d0389fb48bd0f496bf3ebe1ef2fc35a7
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-08 14:36:53 -0300

    Refactoring groups

--- src/main/java/com/greff/foodapi/domain/model/State.java
+++ src/main/java/com/greff/foodapi/domain/model/State.java
@@ -1,9 +1,7 @@
 package com.greff.foodapi.domain.model;
 
-import com.greff.foodapi.core.validation.Groups;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,7 +11,7 @@ import lombok.EqualsAndHashCode;
 @Table(name = "tb_states")
 public class State {
 
-    @NotNull(groups = Groups.StateId.class)
+    //@NotNull(groups = Groups.StateId.class)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
