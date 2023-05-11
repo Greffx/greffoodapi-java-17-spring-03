8261cf237615f3fbffd161d3f45c97e024109a5c
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-11 15:18:51 -0300

    Adding policy to target and qualifiedName property to use methods attributes and uses another mappers classes too

--- src/main/java/com/greff/foodapi/domain/mapper/RestaurantMapper.java
+++ src/main/java/com/greff/foodapi/domain/mapper/RestaurantMapper.java
@@ -3,15 +3,24 @@ package com.greff.foodapi.domain.mapper;
 import com.greff.foodapi.api.model.request.RestaurantRequest;
 import com.greff.foodapi.api.model.response.RestaurantResponse;
 import com.greff.foodapi.domain.model.Restaurant;
-import org.mapstruct.*;
-import org.springframework.stereotype.Service;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring") //@Mapper - annotation tha marks interface as mapping interface to spring
-@Service
+@Mapper(componentModel = "spring",
+        uses = {KitchenMapper.class, CityMapper.class, PaymentMethodMapper.class},
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+//unmappedTargetPolicy = ReportingPolicy.IGNORE, that means policy to just ignore target properties that got null, empty mapping or are not mapped
+//The componentModel = "spring" attribute indicates that the generated mapper implementation class
+//should be registered as a Spring component and can be autowired into other Spring components.
 public interface RestaurantMapper {
 
-    @Mapping(target = "kitchenResponse", source = "kitchen")
-    @Mapping(target = "active", source = "active")
+    @Mapping(target = "addressResponse", source = "address")
+    @Mapping(target = "kitchenResponse", source = "kitchen", qualifiedByName = "toKitchenResponse")
+    @Mapping(target = "addressResponse.cityResponse", source = "address.city", qualifiedByName = "toCityResponse")
+    //qualifiedByName used to specify the name of a specific method that should be used to map a particular source or target property.
+    //better than using several @mapping to each property
     RestaurantResponse fromRestaurantToRestaurantResponse(Restaurant restaurant); //method to transform entity to DTO representation
     //since those objects got different names, need to map target and source to change values
 
@@ -19,8 +28,8 @@ public interface RestaurantMapper {
     Restaurant fromRestaurantRequestToRestaurant(RestaurantRequest restaurant);
 
     @Mapping(target = "kitchen.id", source = "kitchen.id")
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "creationDate", ignore = true)
+    @Mapping(target = "id", ignore = true) //need to ignore, because it will try to change instance id
+    @Mapping(target = "creationDate", ignore = true) //will try to change this property to null
     Restaurant updateDomainObjectByCopying(Restaurant restaurantToCopy, @MappingTarget Restaurant restaurant);
     //Declares a parameter of a mapping method to be the target of the mapping.
     //Not more than one parameter can be declared as MappingTarget.
