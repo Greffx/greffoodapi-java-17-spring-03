a6ee0d703c5213c36c28de38a6db59da4c86ca36
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-11 20:27:37 -0300

    Adding one more method and refactoring

--- src/main/java/com/greff/foodapi/domain/mapper/CityMapper.java
+++ src/main/java/com/greff/foodapi/domain/mapper/CityMapper.java
@@ -2,19 +2,23 @@ package com.greff.foodapi.domain.mapper;
 
 import com.greff.foodapi.api.model.request.CityRequest;
 import com.greff.foodapi.api.model.response.CityResponse;
+import com.greff.foodapi.api.model.response.SimpleCityResponse;
 import com.greff.foodapi.domain.model.City;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", uses = {StateMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", uses = StateMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CityMapper {
 
     @Mapping(target = "state.id", source = "stateIdRefRequest.id")
     City fromCityRequestToCity(CityRequest cityRequest);
 
-    @Named("toCityResponse")
     @Mapping(target = "stateResponse", source = "state", qualifiedByName = "ToStateResponse")
     CityResponse fromCityToCityResponse(City city);
+
+    @Named("toSimpleCityResponse")
+    @Mapping(target = "state", source = "state.name")
+    SimpleCityResponse fromCityToSimpleCityResponse(City city);
 }
