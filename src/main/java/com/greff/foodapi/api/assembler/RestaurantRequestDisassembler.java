d1b94ef057ce64c746829754b91e848d87be587e
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-10 23:47:34 -0300

    Refactoring by removing copy method

--- src/main/java/com/greff/foodapi/api/assembler/RestaurantRequestDisassembler.java
+++ src/main/java/com/greff/foodapi/api/assembler/RestaurantRequestDisassembler.java
@@ -2,7 +2,6 @@ package com.greff.foodapi.api.assembler;
 
 import com.greff.foodapi.api.model.request.RestaurantRequest;
 import com.greff.foodapi.domain.mapper.RestaurantMapper;
-import com.greff.foodapi.domain.model.Kitchen;
 import com.greff.foodapi.domain.model.Restaurant;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,16 +15,5 @@ public class RestaurantRequestDisassembler {
 
     public Restaurant toDomainObject(RestaurantRequest restaurantRequest) {
         return restaurantMapper.fromRestaurantRequestToRestaurant(restaurantRequest);
-    }
-
-    //this one will only copy properties to entity, will not instance a new object domain
-    public void copyToDomainObject(RestaurantRequest restaurantRequest, Restaurant restaurant) {
-        // Resolved [org.springframework.orm.jpa.JpaSystemException: identifier of an instance of com.greff.foodapi.domain.model.Kitchen was altered from 2 to 1]
-        //this problem happens when trying to copy something, JPA understands that we are trying to transform kitchen instance id 1 to id 2.
-        //but we're only trying to change instance reference of kitchen in restaurant
-
-        //solution can be a new instance of kitchen, clean one, JPA never managed this one, can change their id, because this instance id don't exist
-        restaurant.setKitchen(new Kitchen());
-        restaurantMapper.updateDomainObejct(restaurantRequest, restaurant);
     }
 }
