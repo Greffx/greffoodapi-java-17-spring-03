a18831fdc32cabe1d68492eebfef2d5a0b342e05
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-11 00:12:40 -0300

    PaymentMethod assembler e disassembler to modify to model or domain object type

---
+++ src/main/java/com/greff/foodapi/api/assembler/PaymentMethodAssembler.java
@@ -0,0 +1,24 @@
+package com.greff.foodapi.api.assembler;
+
+import com.greff.foodapi.api.model.response.PaymentMethodResponse;
+import com.greff.foodapi.domain.mapper.PaymentMethodMapper;
+import com.greff.foodapi.domain.model.PaymentMethod;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Component
+public class PaymentMethodAssembler {
+
+    private final PaymentMethodMapper paymentMethodMapper;
+
+    public PaymentMethodResponse toModel(PaymentMethod paymentMethod) {
+        return paymentMethodMapper.fromPaymentMethodToPaymentMethodResponse(paymentMethod);
+    }
+
+    public List<PaymentMethodResponse> toCollectionModel(List<PaymentMethod> paymentMethods) {
+        return paymentMethods.stream().map(this::toModel).toList();
+    }
+}
---
+++ src/main/java/com/greff/foodapi/api/controller/PaymentMethodController.java
@@ -0,0 +1,59 @@
+package com.greff.foodapi.api.controller;
+
+import com.greff.foodapi.api.assembler.PaymentMethodAssembler;
+import com.greff.foodapi.api.assembler.PaymentMethodRequestDisassembler;
+import com.greff.foodapi.api.model.request.PaymentMethodRequest;
+import com.greff.foodapi.api.model.response.PaymentMethodResponse;
+import com.greff.foodapi.domain.usecase.PaymentMethodService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/payment_methods")
+public class PaymentMethodController {
+
+    private final PaymentMethodService paymentMethodService;
+    private final PaymentMethodAssembler paymentMethodAssembler;
+    private final PaymentMethodRequestDisassembler paymentMethodRequestDisassembler;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PaymentMethodResponse create(@RequestBody @Valid PaymentMethodRequest paymentMethodRequest) {
+        var paymentMethod = paymentMethodRequestDisassembler.toDomainObject(paymentMethodRequest);
+        var paymentMethodResponse = paymentMethodService.create(paymentMethod);
+
+        return paymentMethodAssembler.toModel(paymentMethodResponse);
+    }
+
+    @PutMapping("/{id}")
+    public PaymentMethodResponse update(@RequestBody @Valid PaymentMethodRequest paymentMethodRequest, @PathVariable Long id) {
+        var paymentMethod = paymentMethodRequestDisassembler.toDomainObject(paymentMethodRequest);
+        var paymentMethodResponse = paymentMethodService.update(paymentMethod, id);
+
+        return paymentMethodAssembler.toModel(paymentMethodResponse);
+    }
+
+    @GetMapping("/{id}")
+    public PaymentMethodResponse findById(@PathVariable Long id) {
+        var paymentMethod = paymentMethodService.findById(id);
+
+        return paymentMethodAssembler.toModel(paymentMethod);
+    }
+
+    @GetMapping
+    public List<PaymentMethodResponse> findAll() {
+        var paymentMethods = paymentMethodService.findAll();
+
+        return paymentMethodAssembler.toCollectionModel(paymentMethods);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        paymentMethodService.deleteById(id);
+    }
+}
