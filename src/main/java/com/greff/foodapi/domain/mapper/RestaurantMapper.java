d088490147ed13a966b18a109008612273398152
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-11 20:27:17 -0300

    Altering update mapper domain object method

--- src/main/java/com/greff/foodapi/domain/mapper/RestaurantMapper.java
+++ src/main/java/com/greff/foodapi/domain/mapper/RestaurantMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
-        uses = {KitchenMapper.class, CityMapper.class, PaymentMethodMapper.class},
+        uses = {KitchenMapper.class, CityMapper.class, PaymentMethodMapper.class, AddressMapper.class},
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 //The componentModel = "spring" attribute indicates that the generated mapper implementation class
 //should be registered as a Spring component and can be autowired into other Spring components.
@@ -18,20 +18,21 @@ import org.mapstruct.ReportingPolicy;
 //unmappedTargetPolicy = ReportingPolicy.IGNORE, that means policy to just ignore target properties that got null, empty mapping or are not mapped
 public interface RestaurantMapper {
 
-    @Mapping(target = "addressResponse", source = "address")
+    //since those objects got different names, need to map target and source to change values
+    @Mapping(target = "addressResponse", source = "address", qualifiedByName = "toAddressResponse")
     @Mapping(target = "kitchenResponse", source = "kitchen", qualifiedByName = "toKitchenResponse")
-    @Mapping(target = "addressResponse.cityResponse", source = "address.city", qualifiedByName = "toCityResponse")
     //qualifiedByName used to specify the name of a specific method that should be used to map a particular source or target property.
     //better than using several @mapping to each property
     RestaurantResponse fromRestaurantToRestaurantResponse(Restaurant restaurant); //method to transform entity to DTO representation
     //since those objects got different names, need to map target and source to change values
 
     @Mapping(target = "kitchen.id", source = "kitchenIdRefRequest.id")
-    Restaurant fromRestaurantRequestToRestaurant(RestaurantRequest restaurant);
+    @Mapping(target = "address", source = "addressRequest", qualifiedByName = "toAddress")
+    Restaurant fromRestaurantRequestToRestaurant(RestaurantRequest restaurantRequest);
 
-    @Mapping(target = "id", ignore = true) //need to ignore, because it will try to change instance id
-    @Mapping(target = "creationDate", ignore = true) //will try to change this property to null
-    Restaurant updateDomainObjectByCopying(Restaurant restaurantToCopy, @MappingTarget Restaurant restaurant);
+    @Mapping(target = "kitchen.id", source = "kitchenIdRefRequest.id")
+    @Mapping(target = "address.city.id", source = "addressRequest.cityIdRefRequest.id")
+    void updateDomainObject(RestaurantRequest restaurantRequest, @MappingTarget Restaurant restaurant);
     //Declares a parameter of a mapping method to be the target of the mapping.
     //Not more than one parameter can be declared as MappingTarget.
 }
