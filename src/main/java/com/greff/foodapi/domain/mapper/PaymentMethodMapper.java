5403853f985edcfbc842d5a6b25858a8de6916f7
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-11 15:17:25 -0300

    Adding policy to unmapped target to ignore what is not mapped

--- src/main/java/com/greff/foodapi/domain/mapper/PaymentMethodMapper.java
+++ src/main/java/com/greff/foodapi/domain/mapper/PaymentMethodMapper.java
@@ -4,19 +4,15 @@ import com.greff.foodapi.api.model.request.PaymentMethodRequest;
 import com.greff.foodapi.api.model.response.PaymentMethodResponse;
 import com.greff.foodapi.domain.model.PaymentMethod;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.springframework.stereotype.Service;
+import org.mapstruct.ReportingPolicy;
 
-@Service
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PaymentMethodMapper {
 
     PaymentMethodResponse fromPaymentMethodToPaymentMethodResponse(PaymentMethod paymentMethod);
 
-    @Mapping(target = "id", ignore = true)
     PaymentMethod fromPaymentMethodRequestToPaymentMethod(PaymentMethodRequest paymentMethodRequest);
 
-    @Mapping(target = "id", ignore = true)
     PaymentMethod copyPaymentMethodToAnotherPaymentMethodInstance(PaymentMethod paymentMethodToCopy, @MappingTarget PaymentMethod paymentMethod);
 }
