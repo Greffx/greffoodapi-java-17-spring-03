1d8dc254f1c3734547f60ff255b69cb45d7fe73c
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-11 15:16:59 -0300

    Adding name to create qualifier to state response to be used in city mapper, better than using multiple @mapping

--- src/main/java/com/greff/foodapi/domain/mapper/StateMapper.java
+++ src/main/java/com/greff/foodapi/domain/mapper/StateMapper.java
@@ -4,12 +4,16 @@ import com.greff.foodapi.api.model.request.StateRequest;
 import com.greff.foodapi.api.model.response.StateResponse;
 import com.greff.foodapi.domain.model.State;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Service;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
-@Service
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface StateMapper {
-    
+
+    @Named("ToStateResponse")
+    //@Named indicate which method with the same name in the mapper class should be used for a specific mapping.
+    //When using the qualifiedByName on another class method
+    //attributed in @Mapping, you can specify the name of the method to be used as a qualifier.
     StateResponse fromStateToStateResponse(State state);
 
     State fromStateRequestToState(StateRequest stateRequest);
