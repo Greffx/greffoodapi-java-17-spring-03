774e4f88d170a273e13ce8fe21fd09f42ed037e6
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-10 22:11:14 -0300

    Refactoring adding one more constructor with resourceName

--- src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
+++ src/main/java/com/greff/foodapi/domain/usecase/exception/NotFoundObjectException.java
@@ -4,9 +4,13 @@ public abstract class NotFoundObjectException extends BusinessException {
     //we can not instance more this type of exception, serves to help others
     //but can be used in catch, so if needed to catch something more general, can use this
 
+    public NotFoundObjectException(String message) {
+        super(message);
+    }
+
     //Abstract classes should not have public constructors,
     // Constructors of abstract classes can only be called in constructors of their subclasses
-    protected NotFoundObjectException(String message) {
-        super(message);
+    protected NotFoundObjectException(String resourceName, Long id) {
+        super(String.format("%s with id %d, not found", resourceName, id));
     }
 }
