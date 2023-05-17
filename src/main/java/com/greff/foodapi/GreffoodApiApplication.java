c74a6828cb5b234e32e80b5aa661fe9644fe7ed0
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-17 20:50:29 -0300

    Adding jpa base class to all repositories implementations

--- src/main/java/com/greff/foodapi/GreffoodApiApplication.java
+++ src/main/java/com/greff/foodapi/GreffoodApiApplication.java
@@ -1,11 +1,17 @@
 package com.greff.foodapi;
 
+import com.greff.foodapi.infrastructure.repository.impl.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
+//EnableJpaRepositories is saying = should be used as the base class for all your repository implementations
+//This means that all repositories application will extend CustomJpaRepositoryImpl and inherit its functionality
+//including the custom methods or overrides you have defined in that class
 public class GreffoodApiApplication {
 
     public static void main(String[] args) {
