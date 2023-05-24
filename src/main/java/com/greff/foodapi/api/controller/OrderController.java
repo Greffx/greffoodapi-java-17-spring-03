39038735f65ad726b0d64138bbf3e3342742da06
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-24 20:26:02 -0300

    Adding Create endpoint for order

--- src/main/java/com/greff/foodapi/api/controller/OrderController.java
+++ src/main/java/com/greff/foodapi/api/controller/OrderController.java
@@ -1,14 +1,15 @@
 package com.greff.foodapi.api.controller;
 
 import com.greff.foodapi.api.assembler.OrderAssembler;
+import com.greff.foodapi.api.assembler.OrderRequestDisassembler;
+import com.greff.foodapi.api.model.request.OrderRequest;
 import com.greff.foodapi.api.model.response.OrderResponse;
 import com.greff.foodapi.api.model.response.SimpleOrderResponse;
 import com.greff.foodapi.domain.usecase.OrderService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class OrderController {
 
     private final OrderAssembler orderAssembler;
     private final OrderService orderService;
+    private OrderRequestDisassembler orderRequestDisassembler;
 
     @GetMapping("/{id}")
     public OrderResponse findById(@PathVariable Long id) {
@@ -32,6 +34,16 @@ public class OrderController {
         var orders = orderService.findAll();
 
         return orderAssembler.toSimpleCollectionModel(orders);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponse create(@RequestBody @Valid OrderRequest orderRequest) {
+        var order = orderRequestDisassembler.toDomainObject(orderRequest);
+
+        order = orderService.create(order);
+
+        return orderAssembler.toModel(order);
     }
 
 }
