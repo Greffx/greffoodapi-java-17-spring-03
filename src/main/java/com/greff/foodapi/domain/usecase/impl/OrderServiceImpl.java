97a3f138bb591ac2e6f2cf59bc5258072e9e1eeb
Eduardo Greff <eduardogreff0@gmail.com>
2023-05-24 21:00:03 -0300

    Refactoing methods by extracting private void methods to validate and not to do all in one method only, dividing responsibilities

--- src/main/java/com/greff/foodapi/domain/usecase/impl/OrderServiceImpl.java
+++ src/main/java/com/greff/foodapi/domain/usecase/impl/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import com.greff.foodapi.domain.usecase.*;
 import com.greff.foodapi.domain.usecase.exception.BusinessException;
 import com.greff.foodapi.domain.usecase.exception.NotFoundObjectException;
 import com.greff.foodapi.domain.usecase.exception.OrderNotFoundException;
+import com.greff.foodapi.domain.usecase.exception.PaymentMethodNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,43 +39,52 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Order create(Order order) {
+        var restaurantId = order.getRestaurant().getId();
+        var paymentMethodId = order.getPaymentMethod().getId();
+
         try {
-            var restaurant = restaurantService.findById(order.getRestaurant().getId());
-            var paymentMethod = paymentMethodService.findById(order.getPaymentMethod().getId());
-            var city = cityService.findById(order.getAddress().getCity().getId());
-            var user = userService.findById(1L);
+            validatingOrderObjects(order, restaurantId, paymentMethodId);
 
-            order.setRestaurant(restaurant);
-            order.setDeliveryTax(restaurant.getDeliveryTax());
-            order.setPaymentMethod(paymentMethod);
-            order.setStatus(OrderStatus.CREATED);
-            order.setUser(user);
-            order.getAddress().setCity(city);
+            validatingOrderItems(order);
 
-            order.getItems().forEach(eachOrderItem -> {
-                Product product = productService.findProductThroughRestaurant(
-                        order.getRestaurant().getId(), eachOrderItem.getProduct().getId());
-
-                eachOrderItem.setOrder(order);
-                eachOrderItem.setProduct(product);
-                eachOrderItem.setUnityPrice(eachOrderItem.getProduct().getPrice());
-
-                var totalPerOrderItem = eachOrderItem.calculatingSubTotal(
-                        eachOrderItem.getProduct().getPrice(), eachOrderItem.getQuantity());
-
-                eachOrderItem.setTotalPrice(totalPerOrderItem);
-            });
-
-            var subTotalOfAllItems = order.sumOfSubTotalOfAllItems(order.getItems());
-            var totalPrice = order.sumOrderTotalPrice(subTotalOfAllItems, restaurant.getDeliveryTax());
-
-            order.setSubtotal(subTotalOfAllItems);
-            order.setTotal(totalPrice);
+            order.sumOfSubTotalOfAllItems(order.getItems());
+            order.sumOrderTotalPrice();
 
             return orderRepository.save(order);
+
+        } catch (PaymentMethodNotFoundException e) {
+            throw new BusinessException(String.format(
+                    "Restaurant with id %d don't accept Payment with id %d", restaurantId, paymentMethodId));
 
         } catch (NotFoundObjectException e) {
             throw new BusinessException(e.getMessage());
         }
+    }
+
+    private void validatingOrderItems(Order order) {
+        order.getItems().forEach(eachOrderItem -> {
+            Product product = productService.findProductThroughRestaurant(
+                    order.getRestaurant().getId(), eachOrderItem.getProduct().getId());
+
+            eachOrderItem.setOrder(order);
+            eachOrderItem.setProduct(product);
+            eachOrderItem.setUnityPrice(product.getPrice());
+
+            eachOrderItem.calculatingSubTotal();
+        });
+    }
+
+    private void validatingOrderObjects(Order order, Long restaurantId, Long paymentMethodId) {
+        var restaurant = restaurantService.findById(restaurantId);
+        var paymentMethod = paymentMethodService.findById(paymentMethodId);
+        var city = cityService.findById(order.getAddress().getCity().getId());
+        var user = userService.findById(1L);
+
+        order.setRestaurant(restaurant);
+        order.setDeliveryTax(restaurant.getDeliveryTax());
+        order.setPaymentMethod(paymentMethod);
+        order.setStatus(OrderStatus.CREATED);
+        order.setUser(user);
+        order.getAddress().setCity(city);
     }
 }
